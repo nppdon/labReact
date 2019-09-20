bc2736573f2d16b1e43563be82e1d9e87f7c4ffb
Noppadol Bilsanheem <noppadol.th@outlook.com>
2019-09-21 01:08:09 +0700

    GAME LOGIC: add category of words

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -3,8 +3,27 @@ import './App.css';
 //import CharacterCard from './CharacterCard';
 import WordCard from './WordCard';
 
-var words = ['Rock', 'Paper', 'Scissor'];
-var word = words[Math.floor(Math.random()*words.length)];
+var categories = ['VEGTABLE', 'COLOR', 'ANIMAL','CARTOON'];
+var randomnum=Math.floor(Math.random()*categories.length)
+var category = categories[randomnum];
+
+if(randomnum==0){
+  var VEGTABLE = ['pepper', 'mushroom', 'carrot'];
+  var word = VEGTABLE[Math.floor(Math.random()*VEGTABLE.length)];}
+
+if(randomnum==1){
+  var COLOR = ['pink', 'blue', 'green'];
+  var word = COLOR[Math.floor(Math.random()*COLOR.length)];}
+
+if(randomnum==2){
+  var ANIMAL = ['duck', 'zebra', 'monkey'];
+  var word = ANIMAL[Math.floor(Math.random()*ANIMAL.length)];}
+
+if(randomnum==3){
+var CARTOON = ['benten', 'conan', 'tom&jerry'];
+var word = CARTOON[Math.floor(Math.random()*CARTOON.length)];}
+
+
 
 //const word = "Hello";
 
@@ -13,7 +32,7 @@ class App extends Component{
     return(
       <div>
         {
-          <WordCard value={word}/>
+          <WordCard value={word} category={category}/>
           //Array.from(word).map((c,i) => <CharacterCard value={c} key={i}/>)
         }
       </div>
--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -53,6 +53,7 @@ export default class WordCard extends Component{
     render(){
         return (
             <div>
+                <h1>CATEGORY : {this.props.category}</h1>
                 { Array.from(this.state.chars).map((c,i) => <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler}/>)}
                 <h1> Round : {this.state.attempt}</h1>
                 <h3> You can play maximum at {this.state.playMax} times</h3>
