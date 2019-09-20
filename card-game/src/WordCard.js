36d861cf8bade499a8789e1167f7d31849b8495e
Noppadol Bilsanheem <noppadol.th@outlook.com>
2019-09-21 00:23:48 +0700

    GAME LOGIC : add maximum time to play
    you can play at 5 times that is maximum

--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -10,7 +10,9 @@ const prepareStateFromWord = (given_word) => {
         chars,
         attempt: 1,
         guess: [],
-        completed: false
+        completed: false,
+        playMax: 5, // play maximum 5 times
+        gamestatus:false // true when attempted 5 times or when you lose
     }
 }
 export default class WordCard extends Component{
@@ -32,11 +34,18 @@ export default class WordCard extends Component{
         })
 
         if(guess.length === this.state.chars.length){
-            if(guess.join('').toString() === this.state.word){
-                this.setState({guess: [], completed: true})
+            if(this.state.attempt!=this.state.playMax){ 
+                if(guess.join('').toString() === this.state.word){
+                    this.setState({guess: [], completed: true})
+                }
+                else{
+                    this.setState({guess: [], attempt: this.state.attempt + 1})
+                }
             }
             else{
-                this.setState({guess: [], attempt: this.state.attempt + 1})
+                this.setState({guess: [], completed: true})
+                this.setState({guess: [], gamestatus:true})
+
             }
         }
         
@@ -46,7 +55,9 @@ export default class WordCard extends Component{
             <div>
                 { Array.from(this.state.chars).map((c,i) => <CharacterCard value={c} key={i} attempt={this.state.attempt} activationHandler={this.activationHandler}/>)}
                 <h1> Round : {this.state.attempt}</h1>
-                <h1>{this.state.completed? 'YOU WIN' : ''}</h1>
+                <h3> You can play maximum at {this.state.playMax} times</h3>
+                <h1>{this.state.completed&&!this.state.gamestatus? 'YOU WIN' : ''}</h1>
+                <h1>{this.state.gamestatus? 'YOU LOSE' : ''}</h1>
             </div>
         );
     }
